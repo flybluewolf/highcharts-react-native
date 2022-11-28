617fda38da63432da8542c01e6a314a63c6c1df8
jrconsole <jrconsole@gmail.com>
2022-11-27 21:43:58 -0600

    upgrade to expo 45

--- App.js
+++ App.js
@@ -1,79 +1,77 @@
 import React from 'react';
-import {
-    StyleSheet,
-    View,
-    Button
-} from 'react-native';
-import HighchartsReactNative from '@highcharts/highcharts-react-native';
+import { StyleSheet, View, Button } from 'react-native';
+import HighchartsReactNative from 'highcharts-dist';
 
 const modules = [
-    //'solid-gauge'
+  //'solid-gauge'
 ];
 
 export default class App extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            chartOptions: {
-                chart: {
-                    events: {
-                        load() {
-                            alert(window.Highcharts.version)
-                        }
-                    }
-                },
-                title: {
-                    text: 'Default title'
-                },
-                series: [{
-                    data: [1, 3, 2]
-                }]
-            }
-        };
-    }
+    this.state = {
+      chartOptions: {
+        chart: {
+          events: {
+            load() {
+              alert(window.Highcharts.version);
+            },
+          },
+        },
+        title: {
+          text: 'Default title',
+        },
+        series: [
+          {
+            data: [1, 3, 2],
+          },
+        ],
+      },
+    };
+  }
 
-    chartUpdate() {
-        this.setState({
-            chartOptions: {
-                chart: {
-                    type: 'column'
-                },
-                title: {
-                    text: 'Updated chart'
-                }
-            }
-        });
-    }
+  chartUpdate() {
+    this.setState({
+      chartOptions: {
+        chart: {
+          type: 'column',
+        },
+        title: {
+          text: 'Updated chart',
+        },
+      },
+    });
+  }
 
-    render() {
-        return (
-            <View style={ styles.container }>
-                <HighchartsReactNative
-                    //useCDN={true}
-                    styles={styles.container}
-                    options={this.state.chartOptions}
-                    devPath={'192.168.0.1:12345'}
-                    //useSSL={true}
-                    modules={modules}
-                />
-                <Button
-                    onPress={this.chartUpdate.bind(this)}
-                    style={styles.button}
-                    title='Chart update'
-                    color='#000'
-                />
-            </View>
-        );
-    }
+  render() {
+    return (
+      <View style={styles.container}>
+        <HighchartsReactNative
+          //useCDN={true}
+          styles={styles.container}
+          options={this.state.chartOptions}
+          devPath={'192.168.0.1:12345'}
+          //useSSL={true}
+          modules={modules}
+        />
+        <Button
+          onPress={this.chartUpdate.bind(this)}
+          style={styles.button}
+          title="Chart update"
+          color="#000"
+        />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        justifyContent: 'center',
-        flex: 1
-    },
-    button: {
-        justifyContent: 'center'
-    }
+  container: {
+    justifyContent: 'center',
+    flex: 1,
+  },
+  button: {
+    justifyContent: 'center',
+  },
 });
